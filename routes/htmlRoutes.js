9be84ecfe4f825d8d3bcf332e77bdcf91400e228
Mariah Rivera <rivmariah06@gmail.com>
2021-03-19 17:25:16 -0400

    adding in html route for exercise.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,35 +1,7 @@
-const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+var path = require("path");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-module.exports = router;
+module.exports = function (app) {
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+};
